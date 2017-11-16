e62fa39c594d84fbe03c4ab19d862ccafa66ac99
Petar <petar.kovacevic@outlook.com>
2017-11-16 10:59:24 +0100

    Added error logging to Student app

--- Lec04-AspNetCore2/Demo01_StudentsAppEntityFramework6/Program.cs
+++ Lec04-AspNetCore2/Demo01_StudentsAppEntityFramework6/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace Demo01_StudentsAppEntityFramework6
 {
@@ -13,6 +15,23 @@ namespace Demo01_StudentsAppEntityFramework6
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSerilog((context, logger) =>
+                {
+                    var cnnstr = context.Configuration["ConnectionString"];
+
+                    logger.MinimumLevel.Error()
+                        .Enrich.FromLogContext()
+                        .WriteTo.MSSqlServer(
+                            connectionString: cnnstr,
+                            tableName: "Errors",
+                            autoCreateSqlTable: true);
+
+                    if (context.HostingEnvironment.IsDevelopment())
+                    {
+                        // Additionally, write to file only in development mode
+                        logger.WriteTo.RollingFile("error-log.txt");
+                    }
+                })
                 .Build();
     }
 }
