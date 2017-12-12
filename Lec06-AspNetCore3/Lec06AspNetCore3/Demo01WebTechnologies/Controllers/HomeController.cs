e5be0c0b28af1fcadc6dda882c302c0a33ab46f0
Petar <petar.kovacevic@outlook.com>
2017-12-12 21:50:06 +0100

    initialized demo

---
+++ Lec06-AspNetCore3/Lec06AspNetCore3/Demo01WebTechnologies/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo01WebTechnologies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
