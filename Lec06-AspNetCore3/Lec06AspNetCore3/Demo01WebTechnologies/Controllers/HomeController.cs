575950339cd1bf72dbe5b2f8b4cc6226dd6ac3fe
Petar <petar.kovacevic@outlook.com>
2017-12-13 12:22:35 +0100

    Demo start

--- Lec06-AspNetCore3/Lec06AspNetCore3/Demo01WebTechnologies/Controllers/HomeController.cs
+++ Lec06-AspNetCore3/Lec06AspNetCore3/Demo01WebTechnologies/Controllers/HomeController.cs
@@ -14,17 +14,8 @@ namespace Demo01WebTechnologies.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
