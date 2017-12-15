cd148f7e713ab272a2152817d3742249f51cec3a
Petar <petar.kovacevic@outlook.com>
2017-12-15 14:38:43 +0100

    demo finish

--- Lec06-AspNetCore3/Lec06AspNetCore3/Demo01WebTechnologies/wwwroot/js/index.js
+++ Lec06-AspNetCore3/Lec06AspNetCore3/Demo01WebTechnologies/wwwroot/js/index.js
@@ -1,1 +1,23 @@
 ﻿// Write your JavaScript code.
+$("#increaseButton").on("click", function() {
+    var counter = $("#counter").html();
+    counter = parseInt($("#counter").html()) + 1;
+    $("#counter").html(counter);
+});
+
+$("#showHideButton").on("mouseenter",
+    function() {
+        $("#counterContainer").hide();
+    });
+
+$("#showHideButton").on("mouseleave",
+    function () {
+        $("#counterContainer").show();
+    });
+$("#renderButton").on("click",
+    function() {
+        $.get("/home/getstudents",
+            function(students) {
+
+            });
+    });
